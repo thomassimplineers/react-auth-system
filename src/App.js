47751529b54c69c44b94873db5b4e635fff70adf
thomassimplineers <thomas@simplineers.se>
2024-12-03 11:22:11 +0100

    Simplify App.js to show Login component

--- src/App.js
+++ src/App.js
@@ -1,25 +1,10 @@
-import { AuthProvider } from './components/AuthProvider';
 import Login from './components/Login';
-import { useAuth } from './components/AuthProvider';
-
-const AuthenticatedApp = () => {
-  return (
-    <div className="p-4">
-      <h1>Welcome to your dashboard!</h1>
-      {/* Add your authenticated app content here */}
-    </div>
-  );
-};
 
 function App() {
-  const { user } = useAuth();
-
   return (
-    <AuthProvider>
-      <div className="App">
-        {user ? <AuthenticatedApp /> : <Login />}
-      </div>
-    </AuthProvider>
+    <div className="App">
+      <Login />
+    </div>
   );
 }
 
